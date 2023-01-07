f9e089973c1c6b35b6fb75376d1aa6cd71673a9a
davidthomasparks <Davidthomasparks@gmail.com>
2023-01-07 12:51:44 -0800

    Specify height and width for user avatar

--- components/dashboard/user-avatar.tsx
+++ components/dashboard/user-avatar.tsx
@@ -1,11 +1,8 @@
 "use client"
 
-import { User } from "@prisma/client"
+import { Avatar } from "@/ui/avatar"
 import { AvatarProps } from "@radix-ui/react-avatar"
 import { TUser } from "./user-account-nav"
-import { Icons } from "@/components/icons"
-import { Avatar } from "@/ui/avatar"
-import { getBaseUrl } from "@/lib/utils"
 
 interface UserAvatarProps extends AvatarProps {
     user: TUser
@@ -22,6 +19,8 @@ export function UserAvatar({ user, ...props }: UserAvatarProps) {
     return (
         <Avatar {...props}>
             <img
+                height={32}
+                width={32}
                 alt={`${user.name} image`}
                 src={`/api/avatar?seed=${user.id}`}
             />
