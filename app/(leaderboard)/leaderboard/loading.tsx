c9884c0327df6f531b278ea2d48d147342f7f84f
davidthomasparks <Davidthomasparks@gmail.com>
2023-01-08 15:43:48 -0800

    Fix suspense loading on leaderboard page

--- app/(leaderboard)/leaderboard/loading.tsx
+++ app/(leaderboard)/leaderboard/loading.tsx
@@ -8,10 +8,10 @@ export default function NewLoading() {
                 <div className="mx-auto  px-4 sm:px-6 lg:px-8">
                     <div className="text-center">
                         <h1 className="text-lg font-semibold text-rose-600">
-                            Leaderboard
+                            Make your mark
                         </h1>
                         <h1 className="mt-1 text-4xl font-bold tracking-tight text-brandtext-500 sm:text-5xl lg:text-6xl flex gap-8 justify-center w-full items-center">
-                            Make your mark
+                            Leaderboard
                         </h1>
                         <p className="mx-auto mt-5 max-w-xl text-xl text-brandtext-600">
                             Collect Blood and rise to the top of the Vamps!
