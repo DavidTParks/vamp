46c38b2abcd139767aabe0652cc5c96ea9f7eacf
davidthomasparks <Davidthomasparks@gmail.com>
2023-01-07 19:08:41 -0800

    Fix weird issue with head.tsx that was causing routing to throw

--- app/head.tsx
+++ app/head.tsx
@@ -8,44 +8,9 @@ export default function Head() {
             />
             <meta
                 name="description"
-                content="Vamp is an open-source platform to empower open source maintainers, and the contributors that make them great."
+                content="Vamp is an open-source platform for projects to post issue bounties and feature requests, and for open-source contributors to resolve them."
             />
             <link rel="icon" href="/vamp.svg" />
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={"Vamp | Open Source Bounties"} />
-            <meta
-                property="og:description"
-                content={
-                    "Vamp is an open-source platform to empower open source maintainers, and the contributors that make them great."
-                }
-            />
-            <meta property="og:url" content={"https://www.vamp.sh"} />
-            <meta
-                property="og:image"
-                content={
-                    "https://jpxwqgklwwytoznbpbmn.supabase.co/storage/v1/object/public/vamp/thumbnail.png"
-                }
-            />
-            <meta
-                name="twitter:title"
-                content={"Vamp | Open Source Bounties"}
-            />
-            <meta
-                name="twitter:description"
-                content={
-                    ' "Vamp is an open-source platform to empower open source maintainers, and the contributors that make them great."'
-                }
-            />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content={"https://www.vamp.sh"} />
-            <meta
-                name="twitter:image"
-                content={
-                    "https://jpxwqgklwwytoznbpbmn.supabase.co/storage/v1/object/public/vamp/thumbnail.png"
-                }
-            />
         </>
     )
 }
