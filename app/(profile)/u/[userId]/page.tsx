87d0f5e0a386e799f9b239208fa77f50c4c66620
davidthomasparks <Davidthomasparks@gmail.com>
2023-01-16 12:10:33 -0800

    Add tailwind animate utilities, fix profile page nav z index

--- app/(profile)/u/[userId]/page.tsx
+++ app/(profile)/u/[userId]/page.tsx
@@ -78,17 +78,23 @@ export default async function ProjectPage({
                                     {user.name}{" "}
                                 </h1>
                                 <div>
-                                    <div className="mt-3 flex w-fit flex-none items-center justify-center gap-1 overflow-hidden rounded-full border border-zinc-700/50 bg-zinc-800 px-3 py-1 shadow-md  shadow-zinc-800/5 ring-0 ring-zinc-900/5 sm:m-3 sm:flex sm:hidden ">
-                                        <span className="font-display text-sm font-bold tracking-tight text-brandtext-500">
-                                            x{user.blood}
-                                        </span>
-                                        <Image
-                                            alt="potion"
-                                            height={24}
-                                            width={24}
-                                            src="/achievements/blood.png"
-                                        />
-                                    </div>
+                                    <Tooltip
+                                        content={
+                                            "A users Blood score reflects how many bounty submissions they have had accepted."
+                                        }
+                                    >
+                                        <div className="mt-3 flex w-fit flex-none items-center justify-center gap-1 overflow-hidden rounded-full border border-zinc-700/50 bg-zinc-800 px-3 py-1 shadow-md  shadow-zinc-800/5 ring-0 ring-zinc-900/5 sm:m-3 sm:flex sm:hidden ">
+                                            <span className="font-display text-sm font-bold tracking-tight text-brandtext-500">
+                                                x{user.blood}
+                                            </span>
+                                            <Image
+                                                alt="potion"
+                                                height={24}
+                                                width={24}
+                                                src="/achievements/blood.png"
+                                            />
+                                        </div>
+                                    </Tooltip>
                                 </div>
 
                                 {/* <div className="w-full mt-0 mb-2 h-4  rounded-md overflow-hidden relative border border-zinc-700/50 ">
@@ -147,7 +153,7 @@ export default async function ProjectPage({
                         </div>
                     </div>
                     <div className="col-span-4 w-full lg:col-span-8">
-                        <header className=" sticky top-0 left-0 right-0 z-30 border-b border-palette-300 bg-appbg">
+                        <header className=" top-0 left-0 right-0 border-b border-palette-300 bg-appbg">
                             <div className="mx-auto max-w-screen-xl">
                                 <div className="flex h-12 items-center justify-between">
                                     <ProfileTabNav />
--- ui/leaflet.tsx
+++ ui/leaflet.tsx
@@ -38,7 +38,7 @@ export default function Leaflet({
             <motion.div
                 ref={leafletRef}
                 key="leaflet"
-                className="group fixed inset-x-0 bottom-0 z-40 w-screen cursor-grab bg-white pb-5 active:cursor-grabbing sm:hidden"
+                className="group fixed inset-x-0 bottom-0 z-40 w-screen cursor-grab bg-appbg pb-5 text-brandtext-500 active:cursor-grabbing sm:hidden"
                 initial={{ y: "100%" }}
                 animate={controls}
                 exit={{ y: "100%" }}
@@ -50,7 +50,7 @@ export default function Leaflet({
                 dragConstraints={{ top: 0, bottom: 0 }}
             >
                 <div
-                    className={`rounded-t-4xl -mb-1 flex h-7 w-full items-center justify-center border-t border-gray-200`}
+                    className={`rounded-t-4xl -mb-1 flex h-7 w-full items-center justify-center border-t border-raised-border`}
                 >
                     <div className="-mr-1 h-1 w-6 rounded-full bg-gray-300 transition-all group-active:rotate-12" />
                     <div className="h-1 w-6 rounded-full bg-gray-300 transition-all group-active:-rotate-12" />
